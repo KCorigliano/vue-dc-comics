489d14bdfb358e9c5ee5477095562995e246cf94
KCorigliano <corigliano98@hotmail.com>
2022-01-27 14:12:28 +0100

    Jumbotron

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="header">
-        <div class="header-logo">
-            <img src="@/assets/dc-logo.png" alt="">
-        </div>
-        <div class="header-right">
-            <span>CHARACTERS</span>
-            <span>COMICS</span>
-            <span>MOVIES</span>
-            <span>TV</span>
-            <span>GAMES</span>
-            <span>COLLECTIBLES</span>
-            <span>VIDEOS</span>
-            <span>FANS</span>
-            <span>NEWS</span>
-            <span>SHOP</span>
-        </div>
+        <nav>
+            <div class="header-logo">
+                <img src="@/assets/dc-logo.png" alt="">
+            </div>
+            <div class="header-text">
+                <span>CHARACTERS</span>
+                <span>COMICS</span>
+                <span>MOVIES</span>
+                <span>TV</span>
+                <span>GAMES</span>
+                <span>COLLECTIBLES</span>
+                <span>VIDEOS</span>
+                <span>FANS</span>
+                <span>NEWS</span>
+                <span>SHOP</span>
+            </div>
+        </nav>
+        <div class="jumbotron"></div>
     </div>
 </template>
 
@@ -26,19 +29,6 @@ export default {
 
 <style lang="scss" scoped>
 
-.header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 1200px;
-    margin: 0 auto;
-
-    .header-right{
-
-        span{
-            padding: 5px 15px;
-        }
-    }
-}
+@import '@/style/headerBox.scss'
 
 </style>
