52b53ed8db1bb8913ec0e38f6912c7ab5bd9be2f
KCorigliano <corigliano98@hotmail.com>
2022-01-27 15:40:06 +0100

    footer navbar HTML

--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -1,6 +1,22 @@
 <template>
-    <div>
-        
+    <div class="footer-navbar">
+        <div class="container">
+            <div class="footer-navbar-left">
+                <div class="button">
+                    SIGN-UP NOW!
+                </div>
+            </div>
+            <div class="footer-navbar-right">
+                <div>
+                    <p>FOLLOW US</p>
+                    <img src="@/assets/footer-facebook.png" alt="">
+                    <img src="@/assets/footer-twitter.png" alt="">
+                    <img src="@/assets/footer-youtube.png" alt="">
+                    <img src="@/assets/footer-pinterest.png" alt="">
+                    <img src="@/assets/footer-periscope.png" alt="">
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
