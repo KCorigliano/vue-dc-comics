733c9e862432c34cb9ad22e3bc58dbc4be1bbce4
KCorigliano <corigliano98@hotmail.com>
2022-01-27 15:19:44 +0100

    Footer style

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -1,44 +1,45 @@
 <template>
     <div class="footer">
-        <div class="footer-left">
-            <div>
-                <h4>DC COMICS</h4>
-                <p>Characters</p>
-                <p>Comics</p>
-                <p>Movies</p>
-                <p>TV</p>
-                <p>Games</p>
-                <p>Videos</p>
-                <p>News</p>
-                <h4>SHOP</h4>
-                <p>Shop DC</p>
-                <p>Shop DC Collectables</p>
+        <div class="container">
+            <div class="footer-left">
+                <div>
+                    <h3>DC COMICS</h3>
+                    <p>Characters</p>
+                    <p>Comics</p>
+                    <p>Movies</p>
+                    <p>TV</p>
+                    <p>Games</p>
+                    <p>Videos</p>
+                    <p>News</p>
+                    <h3>SHOP</h3>
+                    <p>Shop DC</p>
+                    <p>Shop DC Collectables</p>
+                </div>
+                <div>
+                    <h3>DC</h3>
+                    <p>Termo of use</p>
+                    <p>Privacy policy (New)</p>
+                    <p>Ad Choices</p>
+                    <p>Adverstising</p>
+                    <p>Jobs</p>
+                    <p>Subscriptions</p>
+                    <p>Talent Workshops</p>
+                    <p>CPSC Certificates</p>
+                    <p>Ratings</p>
+                    <p>Shop Help</p>
+                    <p>Contact Us</p>
+                </div>
+                <div>
+                    <h3>SITES</h3>
+                    <p>DC</p>
+                    <p>MAD Magazine</p>
+                    <p>DC Kids</p>
+                    <p>DC Universe</p>
+                    <p>DC Power Visa</p>
+                </div>
             </div>
-            <div>
-                <h4>DC</h4>
-                <p>Termo of use</p>
-                <p>Privacy policy (New)</p>
-                <p>Ad Choices</p>
-                <p>Adverstising</p>
-                <p>Jobs</p>
-                <p>Subscriptions</p>
-                <p>Talent Workshops</p>
-                <p>CPSC Certificates</p>
-                <p>Ratings</p>
-                <p>Shop Help</p>
-                <p>Contact Us</p>
+            <div class="footer-right">
             </div>
-            <div>
-                <h4>SITES</h4>
-                <p>DC</p>
-                <p>MAD Magazine</p>
-                <p>DC Kids</p>
-                <p>DC Universe</p>
-                <p>DC Power Visa</p>
-            </div>
-        </div>
-        <div class="footer-right">
-            <img src="@/assets/dc-logo-bg.png" alt="">
         </div>
     </div>
 </template>
@@ -50,5 +51,5 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-
+@import '@/style/footerBox.scss'
 </style>
