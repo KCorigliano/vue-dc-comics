2fb40a569540a076eeb320779c6f8506f788f5f6
KCorigliano <corigliano98@hotmail.com>
2022-01-27 16:01:17 +0100

    Footer navbar style

--- src/App.vue
+++ src/App.vue
@@ -28,8 +28,16 @@ export default {
 </script>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
+*{
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/components/FooterNav.vue
+++ src/components/FooterNav.vue
@@ -2,19 +2,21 @@
     <div class="footer-navbar">
         <div class="container">
             <div class="footer-navbar-left">
-                <div class="button">
-                    SIGN-UP NOW!
-                </div>
+                <button>SIGN-UP NOW!</button>
             </div>
             <div class="footer-navbar-right">
                 <div>
                     <p>FOLLOW US</p>
+                </div>
+                <div>
                     <img src="@/assets/footer-facebook.png" alt="">
                     <img src="@/assets/footer-twitter.png" alt="">
                     <img src="@/assets/footer-youtube.png" alt="">
                     <img src="@/assets/footer-pinterest.png" alt="">
                     <img src="@/assets/footer-periscope.png" alt="">
                 </div>
+                
+                
             </div>
         </div>
     </div>
@@ -27,5 +29,57 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+@import '@/style/variables';
 
+.footer-navbar{
+    background-color: #333;
+
+    .container{
+        display: flex;
+        justify-content: space-between;
+        align-content: center;
+        width: 1200px;
+        margin: 0 auto;
+
+        > div{
+            width: 50%;
+            padding: 30px 0;
+        }
+
+        .footer-navbar-left{
+            display: flex;
+
+            button{
+                margin: auto 0;
+                color: white;
+                background: #333;
+                font-weight: bold;
+                border: 2px solid $hoverColor;
+                padding: 15px;
+                cursor: pointer;
+            }
+        }
+
+        .footer-navbar-right{
+            display: flex;
+            justify-content: end;
+
+            > div{
+                display: flex;
+                align-content: center;
+            }
+
+            p{
+                color: $hoverColor;
+                font-weight: bold;
+                margin: auto 15px;
+            }
+
+            img{
+                margin: auto 0 auto 15px;
+                cursor: pointer;
+            }
+        }
+    }
+}
 </style>
