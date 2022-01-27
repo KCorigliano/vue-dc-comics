af9ce746e2859038441cb80e343fb259f554b9a3
KCorigliano <corigliano98@hotmail.com>
2022-01-27 14:35:04 +0100

    Main section created

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,19 @@
 <template>
   <div id="app">
     <header-box />
+    <main-content />
   </div>
 </template>
 
 <script>
 import HeaderBox from './components/HeaderBox.vue'
+import MainContent from './components/MainContent.vue'
 
 export default {
   name: 'App',
   components: {
     HeaderBox,
+    MainContent,
   }
 }
 </script>
---
+++ src/components/MainContent.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="main">
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'MainContent',
+}
+</script>
+
+<style lang="scss" scoped>
+.main{
+    height: 500px;
+    background-color: #333;
+}
+</style>
