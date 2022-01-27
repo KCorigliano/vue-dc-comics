e2aaccd66bee76b8122e72794bdf77e07242e9cd
KCorigliano <corigliano98@hotmail.com>
2022-01-27 15:04:11 +0100

    Footer HTML

--- src/components/FooterBox.vue
+++ src/components/FooterBox.vue
@@ -1,6 +1,45 @@
 <template>
     <div class="footer">
-        
+        <div class="footer-left">
+            <div>
+                <h4>DC COMICS</h4>
+                <p>Characters</p>
+                <p>Comics</p>
+                <p>Movies</p>
+                <p>TV</p>
+                <p>Games</p>
+                <p>Videos</p>
+                <p>News</p>
+                <h4>SHOP</h4>
+                <p>Shop DC</p>
+                <p>Shop DC Collectables</p>
+            </div>
+            <div>
+                <h4>DC</h4>
+                <p>Termo of use</p>
+                <p>Privacy policy (New)</p>
+                <p>Ad Choices</p>
+                <p>Adverstising</p>
+                <p>Jobs</p>
+                <p>Subscriptions</p>
+                <p>Talent Workshops</p>
+                <p>CPSC Certificates</p>
+                <p>Ratings</p>
+                <p>Shop Help</p>
+                <p>Contact Us</p>
+            </div>
+            <div>
+                <h4>SITES</h4>
+                <p>DC</p>
+                <p>MAD Magazine</p>
+                <p>DC Kids</p>
+                <p>DC Universe</p>
+                <p>DC Power Visa</p>
+            </div>
+        </div>
+        <div class="footer-right">
+            <img src="@/assets/dc-logo-bg.png" alt="">
+        </div>
     </div>
 </template>
 
