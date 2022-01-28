8f3113826934e2fd09e62c1a998cf68872406eb2
KCorigliano <corigliano98@hotmail.com>
2022-01-28 13:08:21 +0100

    mainContent cards created

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -5,16 +5,7 @@
                 <img src="@/assets/dc-logo.png" alt="">
             </div>
             <div class="header-text">
-                <div><span>CHARACTERS</span></div>
-                <div><span>COMICS</span></div>
-                <div><span>MOVIES</span></div>
-                <div><span>TV</span></div>
-                <div><span>GAMES</span></div>
-                <div><span>COLLECTIBLES</span></div>
-                <div><span>VIDEOS</span></div>
-                <div><span>FANS</span></div>
-                <div><span>NEWS</span></div>
-                <div><span>SHOP</span></div>
+                <div v-for="link in nav" :key="link.id"><a :href="link.url"><span>{{link.name}}</span></a></div>
             </div>
         </nav>
         <div class="jumbotron"></div>
@@ -24,6 +15,9 @@
 <script>
 export default {
     name: 'HeaderBox',
+    props: {
+        nav: Array
+    }
 }
 </script>
 
--- src/components/MainContent.vue
+++ src/components/MainContent.vue
@@ -1,17 +1,60 @@
 <template>
     <div class="main">
+        <div class="container">
+            <div class="card" v-for="(card, index) in cards" :key="index">
+                <div>
+                    <img class="image" :src="card.thumb" alt="">
+                </div>
+                <span>{{card.series}}</span>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
 export default {
     name: 'MainContent',
+    props: {
+        cards: Array
+    }
 }
 </script>
 
 <style lang="scss" scoped>
 .main{
-    height: 500px;
     background-color: #333;
+
+    .container{
+        width: 1200px;
+        display: flex;
+        margin: 0 auto;
+        justify-content: space-around;
+        padding: 50px 0;
+        flex-wrap: wrap;
+
+        .card{
+            width: calc((100% / 6) - 15px);
+            height: 185px;
+            padding: 15px 0;
+            text-align: left;
+            color: white;
+            margin-bottom: 75px;
+
+            div{
+                margin-bottom: 15px;
+
+                .image{
+                    width: 100%;
+                    max-height: 185px;
+                    object-fit: cover;
+                }
+            }
+
+            span{
+                text-transform: uppercase;
+                font-size: 12px;
+            }
+        }
+    }
 }
 </style>
